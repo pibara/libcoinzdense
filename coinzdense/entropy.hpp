569a70d43a322f5326c6c4f6cc7db4b040870374
RJ Meijer <pibara@gmail.com>
2025-08-18 23:41:13 +0200

    minor API update adding value semantics proxy for entropy

--- coinzdense/entropy.hpp
+++ coinzdense/entropy.hpp
@@ -6,6 +6,14 @@ namespace coinzdense {
         virtual ~AbstractEntropy() = default;
         virtual std::basic_string<uint8_t> operator () (uint64_t subkey_id) = 0;
     };
-    std::unique_ptr<AbstractEntropy> make_secret_entropy(std::basic_string<uint8_t> masterkey);
+    struct ValueSemantics: AbstractEntropy {
+        ValueSemantics(std::unique_ptr<AbstractEntropy> ptr): mPtr(std::move(ptr)){}
+        std::basic_string<uint8_t> operator () (uint64_t subkey_id){
+            return (*mPtr)(subkey_id);
+        }
+      private:
+        std::unique_ptr<AbstractEntropy> mPtr;
+    };
+    ValueSemantics make_secret_entropy(std::basic_string<uint8_t> masterkey);
   }
 }
--- entropy.cpp
+++ entropy.cpp
@@ -7,15 +7,15 @@ namespace coinzdense {
         SecretEntropy(std::basic_string<uint8_t> masterkey):mContext("CoinZdns"), mMasterKey(masterkey) {}
 	std::basic_string<uint8_t> operator () (uint64_t subkey_id){
           uint8_t subkey[16];
-	  crypto_kdf_derive_from_key(subkey, 16, subkey_id, mContext.data(), mMasterKey.c_str());
+	  crypto_kdf_derive_from_key(subkey, 16, subkey_id, mContext.data(), mMasterKey.data());
 	  return std::basic_string<uint8_t>(subkey, 16);
 	}
       private:
 	std::string mContext;
 	std::basic_string<uint8_t> mMasterKey;
     };
-    std::unique_ptr<AbstractEntropy> make_secret_entropy(std::basic_string<uint8_t> masterkey){
-        return std::make_unique<SecretEntropy>(masterkey);
+    ValueSemantics make_secret_entropy(std::basic_string<uint8_t> masterkey){
+        return ValueSemantics(std::make_unique<SecretEntropy>(masterkey));
     }
   }
 } 
